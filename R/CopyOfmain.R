6610573e78998ced931de9541edd714d31fce7b7
Antonio Jesus Banegas Luna <ajbanegas@ucam.edu>
2025-03-05 16:44:06 +0100

    Updated documentation

--- R/CopyOfmain.R
+++ R/CopyOfmain.R
@@ -1,6 +1,7 @@
 source("R/SVMFeature.R")
 
-archivo <- "data/prueba2.txt"  # Ruta relativa al directorio de trabajo actual
+datos <- read.table("data/prueba2.txt", sep = "\t", header = TRUE)
+print(datos)
 
 # Definir variables iniciales
 inputs <- paste0('x', 0:277)  # Columnas de entrada
@@ -10,10 +11,10 @@ tam_pob <- 50
 num_fea <- 5
 n_iter <- 10  # Número de iteraciones
 tiempo_max <- 300  # Tiempo máximo en segundos
-modo <- "iteraciones"  # Modo de ejecución: "iteraciones" o "tiempo"
+modo <- "iters"  # Modo de ejecución: "iteraciones" o "tiempo"
 
 # Crear el objeto SVMFeature
-svm_feature <- SVMFeature$new(archivo, inputs, output, costes, tam_pob, num_fea, n_iter, tiempo_max, modo)
+svm_feature <- SVMFeature$new(datos, inputs, output, costes, tam_pob, num_fea, n_iter, tiempo_max, modo)
 
 # Ejecutar el algoritmo
 svm_feature$run()
--- R/prueba.R
+++ R/prueba.R
@@ -1,12 +1,15 @@
 source("R/SVMFeature.R")
-archivo <- "data/prueba.txt"
+
+datos <- read.table("data/prueba.txt", sep = ";", header = TRUE)
+
 inputs <- c('x1', 'x2', 'x3')
 output <- 'y'
 costes <- c(5, 27, 10)
 tam_pob <- 10
 num_fea <- 2
 n_iter <- 10  # Número de iteraciones
-tiempo_max <- 300  # Tiempo máximo en segundos
-modo <- "tiempo"  # Modo de ejecución: "iteraciones" o "tiempo"
-svm_feature <- SVMFeature$new(archivo, inputs, output, costes, tam_pob, num_fea, n_iter, tiempo_max, modo)
+tiempo_max <- 60  # Tiempo máximo en segundos (300)
+modo <- "time"  # Modo de ejecución: "iters" o "time"
+
+svm_feature <- SVMFeature$new(datos, inputs, output, costes, tam_pob, num_fea, n_iter, tiempo_max, modo)
 svm_feature$run()
