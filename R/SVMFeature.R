0797d9028a8dd1509c43b99c7d60175f6770e603
Antonio Jesus Banegas Luna <ajbanegas@ucam.edu>
2025-03-06 15:20:19 +0100

    first version without errors

--- R/SVMFeature.R
+++ R/SVMFeature.R
@@ -1,19 +1,10 @@
-#source("R/Solution.R")
-#source("R/Population.R")
+utils::globalVariables(c("FRONT"))
+utils::globalVariables(c("DIST"))
+utils::globalVariables(c("EPS"))
 
-#usethis::use_package("dplyr")
-#usethis::use_package("ggplot2")
-#usethis::use_package("magrittr")
-library("dplyr")
-library("ggplot2")
-library("magrittr")
-
-
-#' SVMFeature Class
+#' SVMFeature Class (S3 Implementation)
 #'
-#' @description
-#' This class implements a feature selection algorithm using Support Vector Machines (SVM).
-#' It initializes with user-provided data, runs the optimization process, and prints the final population.
+#' Implements a feature selection algorithm using Support Vector Machines (SVM).
 #'
 #' @param data DataFrame. The input data.
 #' @param inputs Character vector. The names of the input features.
@@ -21,170 +12,144 @@ library("magrittr")
 #' @param costs Numeric vector. The costs associated with the features.
 #' @param pop_size Numeric. The population size.
 #' @param num_fea Numeric. The number of features.
-#' @param n_iter Numeric. The number of iterations.
-#' @return An instance of the SVMFeature class.
+#' @param n_iter Numeric. The number of iterations (default: 10).
+#' @param max_time Numeric. The maximum execution time (default: 300 seconds).
+#' @param mode Character. "iters" for iteration-based execution, "time" for time-based execution.
+#' @return An S3 object of class "SVMFeature".
 #' @export
 #'
+#' @importFrom utils head
 #' @importFrom magrittr %>%
 #' @importFrom dplyr group_by summarize n
-#'
-#' @examples
-#' data <- read.table("data/prueba.txt", sep = ";", header = TRUE)
-#' inputs <- c('x1', 'x2', 'x3')
-#' output <- 'y'
-#' costs <- c(5, 27, 10)
-#' pop_size <- 10
-#' num_fea <- 2
-#  n_iter <- 10  # Número de iteraciones
-#  max_time <- 300  # Tiempo máximo en segundos
-#  mode <- "time"  # Modo de ejecución: "iters" o "time"
-#' svm_feature <- SVMFeature$new(archivo, inputs, output, costs, pop_size, num_fea, n_iter, max_time, mode)
-#' svm_feature$run()
-SVMFeature <- setRefClass(
-  "SVMFeature",
-  fields = list(
-    data = "ANY",
-    inputs = "character",
-    output = "character",
-    costs = "numeric",
-    pop_size = "numeric",
-    num_fea = "numeric",
-    n_iter = "numeric",
-    max_time = "numeric",
-    mode = "character",
-    population = "ANY",
-    best_population = "ANY"
-  ),
-  methods = list(
-    #' Initialize the SVMFeature object
-    #'
-    #' @param data DataFrame. The input data.
-    #' @param inputs Character vector. The names of the input features.
-    #' @param output Character. The name of the output variable.
-    #' @param costs Numeric vector. The costs associated with the features.
-    #' @param pop_size Numeric. The population size.
-    #' @param num_fea Numeric. The number of features.
-    #' @param n_iter Numeric. The number of iterations.
-    #' @return An initialized SVMFeature object.
-    initialize = function(data, inputs, output, costs, pop_size, num_fea, n_iter = 10, max_time = 300, mode = "iters") {
-      .self$data <- data
-      .self$inputs <- inputs
-      .self$output <- output
-      .self$costs <- costs
-      .self$pop_size <- pop_size
-      .self$num_fea <- num_fea
-      .self$n_iter <- n_iter
-      .self$max_time <- max_time
-      .self$mode <- mode
+SVMFeature <- function(data, inputs, output, costs, pop_size, num_fea,
+                       n_iter = 10, max_time = 300, mode = "iters") {
 
-      # Normalización de los data
-      scaler <- function(x) {
-        rng <- range(x, na.rm = TRUE)
-        if (rng[1] == rng[2]) {
-          return(rep(0.5, length(x)))  # Si todos los valores son iguales, asignar 0.5 a todos
-        } else {
-          return((x - rng[1]) / (rng[2] - rng[1]))
-        }
-      }
+  # Normalization function
+  scaler <- function(x) {
+    rng <- range(x, na.rm = TRUE)
+    if (rng[1] == rng[2]) return(rep(0.5, length(x)))
+    return((x - rng[1]) / (rng[2] - rng[1]))
+  }
 
-      norm_data <- as.data.frame(lapply(.self$data[, inputs], scaler))
-      norm_data[output] <- .self$data[[output]]
+  # Normalize input data
+  norm_data <- as.data.frame(lapply(data[, inputs], scaler))
+  norm_data[[output]] <- data[[output]]
 
-      # Depuramos los data cargados
-      cat("Normalized data:\n")
-      print(head(norm_data))
+  # Print normalized data
+  cat("Normalized data:\n")
+  print(head(norm_data))
 
-      .self$population <- SVMFeature::Population(data = norm_data, costs = .self$costs, pop_size = .self$pop_size, inputs = .self$inputs, output = .self$output, num_features = .self$num_fea)
-      .self$best_population <- NULL
-    },
-
-    #' Run the SVMFeature optimization process
-    #'
-    #' @return The final population after the optimization process.
-    run = function() {
-      .self$population <- generate_initial_population(.self$population)
-      .self$population <- fnds(.self$population)
-      .self$update_df_solutions()
-
-      .self$best_population <- .self$population
-
-      init_time <- Sys.time()
-
-      run_iteration <- function() {
-        .self$population <- new_population(.self$population)
-        .self$population <- fnds(.self$population)
-
-        reduced_population <- reduce_population(.self$population)
-        reduced_population <- fnds(reduced_population)
-
-        .self$population$solution_list <- reduced_population$solution_list
-        .self$update_df_solutions()
-        rm(reduced_population)
-
-        # Comparar y mantener la mejor población basada en el número de soluciones en el frente 1
-        current_solutions <- nrow(filter(.self$population$df_solutions, FRONT == 1))
-        best_solutions <- nrow(filter(.self$best_population$df_solutions, FRONT == 1))
-
-        if (current_solutions > best_solutions) {
-          .self$best_population <- .self$population
-        }
-      }
-
-      if (.self$mode == "iters") {
-        iter <- 0
-        while (iter < .self$n_iter) {
-          cat(sprintf("ITERATION %d: %f\n", iter, as.numeric(difftime(Sys.time(), init_time, units = "secs"))))
-          run_iteration()
-          iter <- iter + 1
-        }
-      } else if (.self$mode == "time") {
-        while (as.numeric(difftime(Sys.time(), init_time, units = "secs")) < .self$max_time) {
-          cat(sprintf("TIME: %f\n", as.numeric(difftime(Sys.time(), init_time, units = "secs"))))
-          run_iteration()
-        }
-      } else {
-        stop("Invalid execution mode. Usage: 'iters' or 'time'.")
-      }
-
-      print_population(.self$best_population)
-
-      if (is.null(.self$best_population$df_solutions)) {
-        stop("Error: df_solutions is NULL.")
-      }
-
-      front_population1 <- filter(.self$best_population$df_solutions, FRONT == 1)
-
-      if (nrow(front_population1) == 0) {
-        cat("There is no solution in front 1 to plot.\n")
-      } else {
-        front_population1$DIST <- as.numeric(front_population1$DIST)
-        front_population1$EPS <- as.numeric(front_population1$EPS)
-
-        plot <- ggplot(front_population1, aes(x = DIST, y = EPS)) +
-          geom_point(color = 'blue') +
-          labs(x = 'Distance', y = 'Epsilon', title = 'Solutions in Front 1') +
-          theme_minimal() +
-          theme(panel.grid.major = element_line(linewidth = 0.5, linetype = 'solid', colour = "gray"))
-
-        print(plot)
-      }
-    },
-
-    update_df_solutions = function() {
-      df_solutions <- do.call(rbind, lapply(.self$population$solution_list, function(solution) {
-        solution_dict <- to_dict(solution)
-        solution_dict <- lapply(solution_dict, function(item) if (is.list(item)) unlist(item) else item)
-        solution_df <- as.data.frame(t(solution_dict), stringsAsFactors = FALSE)
-        return(solution_df)
-      }))
-      df_solutions$FRONT <- as.numeric(as.character(df_solutions$FRONT))
-      df_solutions <- df_solutions[order(df_solutions$FRONT), ]
-      .self$population$df_solutions <- df_solutions
-
-      # Imprimir número de soluciones por frente
-      fronts_count <- df_solutions %>% group_by(FRONT) %>% summarize(count = n())
-      cat("Number of solutions by front:\n")
-      print(fronts_count)
-    }
+  # Create the S3 object (a list with attributes)
+  object <- list(
+    data = data,
+    inputs = inputs,
+    output = output,
+    costs = costs,
+    pop_size = pop_size,
+    num_fea = num_fea,
+    n_iter = n_iter,
+    max_time = max_time,
+    mode = mode,
+    population = Population(data = norm_data, costs = costs, pop_size = pop_size,
+                            inputs = inputs, output = output, num_features = num_fea),
+    best_population = NULL
   )
-)
+
+  class(object) <- "SVMFeature"
+  return(object)
+}
+
+#' Run the SVMFeature optimization process
+#' @param object An object of class "SVMFeature".
+#' @return The final population after the optimization process.
+#' @export
+run.SVMFeature <- function(object) {
+
+  object$population <- generate_initial_population(object$population)
+  object$population <- fnds(object$population)
+  object <- update_df_solutions(object)
+
+  object$best_population <- object$population
+  init_time <- Sys.time()
+
+  run_iteration <- function() {
+    object$population <- new_population(object$population)
+    object$population <- fnds(object$population)
+
+    reduced_population <- reduce_population(object$population)
+    reduced_population <- fnds(reduced_population)
+
+    object$population$solution_list <- reduced_population$solution_list
+    object <- update_df_solutions(object)
+
+    # Compare and keep the best population based on FRONT == 1
+    current_solutions <- nrow(dplyr::filter(object$population$df_solutions, FRONT == 1))
+    best_solutions <- nrow(dplyr::filter(object$best_population$df_solutions, FRONT == 1))
+
+    if (current_solutions > best_solutions) {
+      object$best_population <- object$population
+    }
+  }
+
+  if (object$mode == "iters") {
+    for (iter in seq_len(object$n_iter)) {
+      cat(sprintf("ITERATION %d: %f seconds\n", iter, as.numeric(difftime(Sys.time(), init_time, units = "secs"))))
+      run_iteration()
+    }
+  } else if (object$mode == "time") {
+    while (as.numeric(difftime(Sys.time(), init_time, units = "secs")) < object$max_time) {
+      cat(sprintf("TIME: %f seconds\n", as.numeric(difftime(Sys.time(), init_time, units = "secs"))))
+      run_iteration()
+    }
+  } else {
+    stop("Invalid execution mode. Use 'iters' or 'time'.")
+  }
+
+  print_population(object$best_population)
+
+  # Plot the Pareto Front if solutions exist
+  if (!is.null(object$best_population$df_solutions)) {
+    front_population1 <- dplyr::filter(object$best_population$df_solutions, FRONT == 1)
+
+    if (nrow(front_population1) > 0) {
+      front_population1$DIST <- as.numeric(front_population1$DIST)
+      front_population1$EPS <- as.numeric(front_population1$EPS)
+
+      plot <- ggplot2::ggplot(front_population1, ggplot2::aes(x = DIST, y = EPS)) +
+        ggplot2::geom_point(color = 'blue') +
+        ggplot2::labs(x = 'Distance', y = 'Epsilon', title = 'Solutions in Front 1') +
+        ggplot2::theme_minimal() +
+        ggplot2::theme(panel.grid.major = ggplot2::element_line(linewidth = 0.5, linetype = 'solid', colour = "gray"))
+
+      print(plot)
+    } else {
+      cat("There is no solution in front 1 to plot.\n")
+    }
+  }
+
+  return(object)
+}
+
+#' Update df_solutions for an SVMFeature object
+#' @param object An object of class "SVMFeature".
+#' @return The updated SVMFeature object.
+#' @export
+update_df_solutions.SVMFeature <- function(object) {
+
+  df_solutions <- do.call(rbind, lapply(object$population$solution_list, function(solution) {
+    solution_dict <- to_dict(solution)
+    solution_dict <- lapply(solution_dict, function(item) if (is.list(item)) unlist(item) else item)
+    as.data.frame(t(solution_dict), stringsAsFactors = FALSE)
+  }))
+
+  df_solutions$FRONT <- as.numeric(as.character(df_solutions$FRONT))
+  df_solutions <- df_solutions[order(df_solutions$FRONT), ]
+  object$population$df_solutions <- df_solutions
+
+  # Print the number of solutions per front
+  fronts_count <- df_solutions %>% dplyr::group_by(FRONT) %>% dplyr::summarize(count = dplyr::n())
+  cat("Number of solutions by front:\n")
+  print(fronts_count)
+
+  return(object)
+}
--- tests/testthat/test_iters.R
+++ tests/testthat/test_iters.R
@@ -11,23 +11,20 @@ test_that("SVMFeature initializes correctly", {
   #expect_null(grid$data_grid)    # Verifica que data_grid es NULL inicialmente
   #expect_null(grid$knot_list)    # Verifica que knot_list es NULL inicialmente
 
-  datos <- data.frame(x1 = 1:4, x2 = 10:14, y = 5:8)
+  datos <- data.frame(x1 = 1:4, x2 = 11:14, y = 5:8)
 
-  # Definir variables iniciales
-  inputs <- c('x1', 'x2')  # Columnas de entrada
-  output <- 'y'  # Columna de salida
-  costs <- rep(1, length(inputs))  # Asignar un coste de 1 a cada característica
-  tam_pob <- 50
-  num_fea <- 5
-  n_iter <- 10  # Número de iteraciones
-  tiempo_max <- 300  # Tiempo máximo en segundos
-  modo <- "iters"  # Modo de ejecución: "iteraciones" o "tiempo"
+  inputs <- c('x1', 'x2')
+  output <- 'y'
+  costs <- rep(1, length(inputs))
+  pop_size <- 50
+  num_fea <- 1
+  n_iter <- 10
+  max_time <- 300
+  mode <- "iters"
 
-  # Crear el objeto SVMFeature
-  svm_feature <- SVMFeature$new(datos, inputs, output, costs, tam_pob, num_fea, n_iter, tiempo_max, modo)
+  svm <- SVMFeature(datos, inputs, output, costs, pop_size, num_fea, n_iter, max_time, mode)
 
-  expect_s3_class(grid, "SVMFeature")  # Verifica que sea de clase SVMFeature
-
-  # Ejecutar el algoritmo
-  #svm_feature$run()
+  expect_s3_class(svm, "SVMFeature")
+  expect_equal(svm$mode, "iters")
+  expect_equal(svm$num_fea, num_fea)
 })
